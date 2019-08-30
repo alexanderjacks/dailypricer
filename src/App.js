0e1ba39e0c193107b80d1e58802adf6235894d27
Alexander Jacks <alexanderthejacks@gmail.com>
2019-08-30 11:20:57 -0700

    :ticket: adds Cognito

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import './App.css';
+import logo from './logo.png';
 import React from 'react';
 import { Header } from 'semantic-ui-react';
-import logo from './logo.png';
-import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
-export default class App extends React.Component {
+class App extends React.Component { 
   render() {
     return (
       <div className="App">
         <Header className="App-header">
           <img src={logo} className="App-logo" alt="dailypricer logo" />
           <p>
-            We use <code>AWS</code> as much as we can
+            Dailypricer runs on as much <code>AWS</code> as possible
           </p>
           <a
             className="App-link"
-            href="https://."
+            href="https://j4cks.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            React is the new HTML
+            Architected &c by Jacks Consulting
           </a>
         </Header>
       </div>
     );
   }
 };
+export default withAuthenticator(App, {includeGreetings: true});
